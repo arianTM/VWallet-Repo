ff5471a3d9be60513c65558151c2561ba56db977
Arian <atapiamotta@gmail.com>
2022-11-12 16:09:53 -0500

    Estilo perfeccionado

--- vwallet-landing/js/header.js
+++ vwallet-landing/js/header.js
@@ -1,10 +1,10 @@
-const headerMenuBar = document.querySelector('.header-menu-bar');
-const openMenu = document.querySelector('.open-menu');
-const closeMenu = document.querySelector('.close-menu');
-const headerMenu = document.querySelector('.header-menu');
+const headerMenuBar = document.querySelector(".header-menu-bar");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+const headerMenu = document.querySelector(".header-menu");
 
-headerMenuBar.addEventListener('click', () => {
-  openMenu.classList.toggle('active');
-  closeMenu.classList.toggle('active');
-  headerMenu.classList.toggle('active');
+headerMenuBar.addEventListener("click", () => {
+  openMenu.classList.toggle("active");
+  closeMenu.classList.toggle("active");
+  headerMenu.classList.toggle("active");
 });
